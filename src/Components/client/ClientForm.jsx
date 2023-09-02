e18f454ee24bde6138b344488418a4d06ae5eebe
FerCorimayo <lucvs177@gmail.com>
2023-09-02 09:16:53 -0300

    el modulo de recarga se quito del formulario

--- src/Components/client/ClientForm.jsx
+++ src/Components/client/ClientForm.jsx
@@ -4,7 +4,7 @@ import Swal from 'sweetalert2'
 const ClientForm = () => {
     const {register, handleSubmit,watch, formState: {errors}}=useForm();
 
-    const saldo= watch("text5")
+    // const saldo= watch("text5")
     const onSubmit = async(data) => {
     
     const da = {
@@ -46,12 +46,16 @@ const ClientForm = () => {
     }
     return (
         <>
-    <div>
+    {/* <div>
       
       <form onSubmit={handleSubmit(onSubmit)} className="grid grid-cols-1 grid-rows-2 gap-4 p-2 bg-gray-200 md:grid-cols-2 md:grid-rows-1 sm:p-3 md:p-5">
       
       <div className="grid grid-cols-2 grid-rows-5 gap-1 p-4 pb-5 m-2 bg-white rounded-md sm:w-auto sm:gap-3 md:gap-4 sm:m-3 md:m-5 sm:p-8 md:p-8 lg:p-16">
-      <h4 className="col-span-2 p-5 pb-0 text-2xl font-bold text-gray-500 border-b border-gray-500 sm:pb-5" >Nuevo Usuario</h4>
+      <h4 className="col-span-2 p-5 pb-0 text-2xl font-bold text-gray-500 border-b border-gray-500 sm:pb-5" >Nuevo Usuario</h4> */}
+        <div className="flex justify-center p-2 bg-gray-400 sm:p-3 md:p-5">
+      
+      <form onSubmit={handleSubmit(onSubmit)} className="grid w-full grid-cols-2 grid-rows-5 gap-1 p-4 pb-5 m-2 bg-white rounded-md sm:w-auto sm:gap-3 md:gap-4 sm:m-3 md:m-5 sm:p-8 md:p-16">
+      
         <div className='col-span-2 row-span-1'>
           <label className="flex flex-row justify-start pt-4 text-sm font-medium leading-6 text-gray-500"> Nombre</label>
           <input
@@ -124,9 +128,9 @@ const ClientForm = () => {
           <div className="flex flex-row justify-end col-span-2">
           <button className="px-5 py-3 m-4 bg-blue-600 rounded-3xl active:bg-blue-900" type='submit'>Enviar</button>
           </div>
-          </div>
+          {/* </div> */}
           
-          <div className="grid w-full grid-cols-1 grid-rows-6 gap-1 p-4 pb-5 m-2 bg-white rounded-md sm:w-auto sm:gap-3 md:gap-4 sm:m-3 md:m-5 sm:p-8 md:p-7 lg:p-16">
+          {/* <div className="grid w-full grid-cols-1 grid-rows-6 gap-1 p-4 pb-5 m-2 bg-white rounded-md sm:w-auto sm:gap-3 md:gap-4 sm:m-3 md:m-5 sm:p-8 md:p-7 lg:p-16">
           <h4 className="col-span-1 row-span-1 p-5 pb-0 text-2xl font-bold text-gray-500 border-b border-gray-500 sm:pb-5" >Saldo</h4>
           <h4 className="col-span-1 row-span-1 p-5 pb-0 text-2xl font-bold text-center text-gray-500 sm:pb-5" >Total</h4>
           <h1 className="col-span-1 row-span-2 p-5 pb-0 font-bold text-center text-gray-300 text-7xl sm:pb-5" >${saldo && !isNaN(saldo) ? parseInt(saldo).toLocaleString('de-DE') : '000'}</h1>
@@ -153,7 +157,7 @@ const ClientForm = () => {
           Actualizar
           </button>
           </div>
-          </div>
+          </div> */}
           
       </form>
       
