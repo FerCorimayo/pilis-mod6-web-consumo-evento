03b597325bf6928108a66c3a7c3e4b814975d8be
Fabian Choque <fabianchoque28@gmail.com>
2023-09-14 22:14:04 -0300

    navegacion para vendedor

--- src/routers/DashboardRoute.jsx
+++ src/routers/DashboardRoute.jsx
@@ -2,10 +2,13 @@ import { Routes, Route } from 'react-router-dom';
 import Navbar from '../components/ui/Navbar';
 import SellerList from '../components/seller/SellerList';
 import SellerForm from '../components/seller/SellerForm';
-import SalesList from '../Components/seller/SalesList';
+import SalesList from '../components/seller/SalesList';
 import ClientList from '../components/client/ClientList';
 import ClientForm from '../components/client/ClientForm';
-import ClientBalance from '../Components/client/ClientBalance';
+import ClientBalance from '../components/client/ClientBalance';
+import GeneratePayment from '../components/seller/GeneratePayment';
+import ValidatePayment from '../components/seller/ValidatePayment';
+import RegisterPayment from '../components/seller/RegisterPayment';
 import { AuthContext } from '../context/AuthContext';
 import { useContext } from 'react';
 
@@ -19,18 +22,23 @@ export const DashboardRoute = () => {
       ( currentUser.role === 'admin' )
           ? <>
             <Navbar navegation={["vendedores","clientes"]}/>
-              <div>
-                  <Routes>
-                      <Route path="vendedores" element={<SellerList />} />
-                      <Route path="vendedores/nuevo" element={<SellerForm />} />
-                      <Route path="vendedores/venta" element={<SalesList />} />
-                      <Route path="clientes" element={<ClientList />} />
-                      <Route path="clientes/nuevo" element={<ClientForm />} />
-                      <Route path="clientes/balance" element={<ClientBalance />} />
-                  </Routes>
-              </div>
+              <Routes>
+                <Route path="vendedores" element={<SellerList />} />
+                <Route path="vendedores/nuevo" element={<SellerForm />} />
+                <Route path="vendedores/venta" element={<SalesList />} />
+                <Route path="clientes" element={<ClientList />} />
+                <Route path="clientes/nuevo" element={<ClientForm />} />
+                <Route path="clientes/balance" element={<ClientBalance />} />
+              </Routes>
           </>
-          : <Navbar navegation={["ventas","pago"]}/>
+          : <>
+            <Navbar navegation={["ventas","cobros"]}/>
+            <Routes>
+                <Route path="cobros" element={<GeneratePayment />} />
+                {/* <Route path="cobros" element={< />} /> */}
+            </Routes>
+          </>
+          
     }
     </>
   )
